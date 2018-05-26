891db07fcf07613922499b2da80c2b35da3f4975
damianjableka <damian.jableka@gmail.com>
2018-05-26 13:23:20 +0200

    simple responive site

---
+++ menu.js
@@ -0,0 +1,16 @@
+﻿menu=[{nazwa:"O Nas",podstrony:[
+{nazwa:"historia",link:"hist.html"},{nazwa:"wydarzenia",link:"wyd.html"},{nazwa:"wystawy",link:"wyst.html"},{nazwa:"zwiedzanie",link:"zwiedz.html"}
+],kolejnosc:0,ilosc:4,link:"#",kolor:"fioletowy"},
+{nazwa:"Seanse",podstrony:[
+{nazwa:"repertuar",link:"rep.html"},{nazwa:"cennik",link:"cennik.html"},{nazwa:"rezerwacja",link:"rezerwacja.html"},{nazwa:"nasze seanse",link:"seanse.html"}
+],kolejnosc:1,ilosc:4,link:"#",kolor:"niebieski"},
+{nazwa:"Edukacja",podstrony:[
+{nazwa:"oferta",link:"oferta.html"},{nazwa:"cennik",link:"cennik_dyd.html"},{nazwa:"rezerwacja",link:"rezerwacja_dyd.html"},{nazwa:"koła zainteresowań",link:"kola.html"},{nazwa:"Olimpiada Astronomiczna",link:"olimpiada.html"}
+],kolejnosc:2,ilosc:5,link:"#",kolor:"zielony"},
+{nazwa:"Galeria",podstrony:[],kolejnosc:3,ilosc:0,link:"galeria.html",kolor:"zolty"},
+{nazwa:"Dojazd",podstrony:[],kolejnosc:4,ilosc:0,link:"dojazd.html",kolor:"pomaranczowy"},
+{nazwa:"Kontakt",podstrony:[],kolejnosc:5,ilosc:0,link:"kontakt.html",kolor:"czerwony"}
+];
+
+
+	 
